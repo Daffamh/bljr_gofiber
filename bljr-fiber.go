606bbfb86a86107288049de838dcd08607092fc7
Daffamh <daffa.muhadzdzab39@smk.belajar.id>
2025-08-05 12:47:50 +0700

    test

--- bljr-fiber.go
+++ bljr-fiber.go
@@ -6,11 +6,56 @@ import (
 	"github.com/gofiber/fiber"
 )
 
+type user struct {
+	Nama   string `json:"nama"`
+	Alamat string `json:"alamat"`
+}
+
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello daffa")
+	app.Get("/user/:id", func(c *fiber.Ctx) {
+		id := c.Params("id")
+		user := new(user)
+
+		if err := c.BodyParser(user); err != nil {
+			c.Status(400).Send("gagal parsing data")
+		}
+		c.Send("GET:menampilkan user dengan nama = " + user.Nama + ", alamat = " + user.Alamat + ", ID = " + id)
+	})
+
+	app.Post("/user", func(c *fiber.Ctx) {
+		user := new(user)
+
+		if err := c.BodyParser(user); err != nil {
+			c.Status(400).Send("gagal presing data")
+		}
+		c.Send("POST:menambahkan user = " + user.Nama + ", alamat = " + user.Alamat)
+	})
+
+	app.Put("user/:id", func(c *fiber.Ctx) {
+		id := c.Params("id")
+		user := new(user)
+
+		if err := c.BodyParser(user); err != nil {
+			c.Status(400).Send("gagal parsing data")
+		}
+		c.Send("PUT: update user ID = " + id + ", nama = " + user.Nama + ", alamat = " + user.Alamat)
+	})
+
+	app.Patch("user/:id", func(c *fiber.Ctx) {
+		id := c.Params("id")
+		user := new(user)
+		if err := c.BodyParser(user); err != nil {
+			c.Status(400).Send("gagal parsing data")
+			return
+		}
+		c.Send("PATCH: update user ID = " + id + ", nama = " + user.Nama + ", alamat = " + user.Alamat)
+	})
+
+	app.Delete("user/:id", func(c *fiber.Ctx) {
+		id := c.Params("id")
+		c.Send("DELETE: menghapus user dengan ID = " + id)
 	})
 
 	fmt.Println("Fiber running on http://localhost:3000")
