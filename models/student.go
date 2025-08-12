9096517239e000637a7c7925fd424fba35c3f753
Daffamh <daffa.muhadzdzab39@smk.belajar.id>
2025-08-12 13:01:09 +0700

    siang

--- models/student.go
+++ models/student.go
@@ -10,6 +10,7 @@ type Student struct {
 	Id        uint        `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name      string      `json:"name"`
 	Address   string      `json:"address"`
+	Email     string      `json:"email" gorm:"unique" validate:"required,email"`
 	BirthDate pgtype.Date `json:"birth_date" gorm:"type:date"`
 	CreatedAt time.Time   `json:"created_at"`
 	UpdatedAt time.Time   `json:"updated_at"`
